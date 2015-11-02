c43b1c8977df366c4fccc70315c26ec83d4126e0
Jason <363608715@qq.com>
2015-11-02 23:38:46 +0800

    加入宏定义，定义外网和内网地址

---
+++ TableViewDemo1/GlobalConst.h
@@ -0,0 +1,15 @@
+//
+//  GlobalConst.h
+//  TableViewDemo1
+//
+//  Created by lulu on 15/11/2.
+//  Copyright © 2015年 cdlulu. All rights reserved.
+//
+
+#ifndef GlobalConst_h
+#define GlobalConst_h
+
+#define OUTSIDEWEBURL "http://newplayer11.oicp.net/"
+#define INSIDEWEBURL "http://192.168.1.111:8001/"
+
+#endif /* GlobalConst_h */
